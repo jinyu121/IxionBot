b75b86c69f5ed6fe89c068cf188ac21b82c2438b
Yu Hao <jinyu121@gmail.com>
2018-02-13 21:34:50 +0800

    `__` to `_`

--- message/remove_keyword_spam.py
+++ message/remove_keyword_spam.py
@@ -7,25 +7,25 @@ from telegram.ext.dispatcher import DispatcherHandlerStop
 
 from util.config import Config
 
-__keyword = []
+_keyword = []
 
 
 def active(dispatcher, group):
-    global __keyword
+    global _keyword
 
     config = Config.get_config("remove_keyword_spam")
     keywork_file = Path(config.get("list", ""))
     if keywork_file.exists():
-        __keyword = [line.strip() for line in keywork_file.open()]
+        _keyword = [line.strip() for line in keywork_file.open()]
 
     dispatcher.add_handler(MessageHandler(Filters.text, func), group=group)
 
 
 def func(bot, update):
-    global __keyword
+    global _keyword
     config = Config.get_config('remove_keyword_spam')
 
-    if any(s in update.message.text for s in __keyword):
+    if any(s in update.message.text for s in _keyword):
         warn_message = config.get('message', "")
         if "" != warn_message.strip():
             bot.send_message(chat_id=update.message.chat_id, text=warn_message)
