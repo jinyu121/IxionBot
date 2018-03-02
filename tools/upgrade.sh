fad52634be3769bc0732b21e6f95f7d1377e5b87
Yu Hao <jinyu121@126.com>
2018-03-02 11:55:37 +0800

    typo

--- tools/upgrade.sh
+++ tools/upgrade.sh
@@ -21,7 +21,7 @@ fi
 echo "${BLUE}Updating Ixion Bot${NORMAL}"
 if git pull --rebase --stat origin master
 then
-    echo "${GREEN}Update Success${NORMAL}\n"
+    echo "${GREEN}Update Success${NORMAL}"
 else
-    echo "${RED}There was an error updating. Try again later?${NORMAL}\n"
+    echo "${RED}There was an error updating. Try again later?${NORMAL}"
 fi
